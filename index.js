2d1612cf4aaea48caa327fc67ce79ca03bd52b21
Aditya Kumar <aditya227192@gmail.com>
2024-09-21 12:28:19 +0530

    index.js modified

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let Nobtn = document.querySelector('.No');
 Nobtn.addEventListener('click', () => {
-    let x = Math.random() * 100 + 20;
-    let y = Math.random() * 100 + 20; 
+    let x = Math.random() * 400 + 1;
+    let y = Math.random() * 200 + 1; 
     Nobtn.style.transform = `translate(${x}px, ${y}px)`;
 });
 
 Nobtn.addEventListener('mouseover', () => {
-    let x = Math.random() * 100 + 20;
-    let y = Math.random() * 100 + 20; 
+    let x = Math.random() * 400 + 1;
+    let y = Math.random() * 200 + 1; 
     Nobtn.style.transform = `translate(${x}px, ${y}px)`;
 });
 
